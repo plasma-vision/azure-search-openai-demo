f79048551e961b19f1e3c868f71fbf5dbf78c6e3
Peter Washburn <47249732+plasma-vision@users.noreply.github.com>
2023-12-11 15:04:49 +0000

    Clean up header. Change favicon

---
+++ app/backend/static/assets/NoPage-3d3300be.js
@@ -0,0 +1,2 @@
+import{bD as o}from"./vendor-bfe9f059.js";function e(){return o.jsx("h1",{children:"404"})}e.displayName="NoPage";export{e as Component};
+//# sourceMappingURL=NoPage-3d3300be.js.map
