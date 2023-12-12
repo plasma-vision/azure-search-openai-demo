f72decc05758206a982a12db2d490828024bdb77
Peter Washburn <47249732+plasma-vision@users.noreply.github.com>
2023-12-12 13:44:10 +0000

    Seeing if I can include the OMAX logo

--- app/frontend/src/pages/chat/Chat.tsx
+++ app/frontend/src/pages/chat/Chat.tsx
@@ -1,6 +1,7 @@
 import { useRef, useState, useEffect } from "react";
 import { Checkbox, Panel, DefaultButton, TextField, SpinButton, Dropdown, IDropdownOption } from "@fluentui/react";
 import { SparkleFilled, ChatHelpFilled } from "@fluentui/react-icons";
+import Logo from "../../assets/omax-technology.svg";
 import readNDJSONStream from "ndjson-readablestream";
 
 import styles from "./Chat.module.css";
@@ -235,7 +236,8 @@ const Chat = () => {
                 <div className={styles.chatContainer}>
                     {!lastQuestionRef.current ? (
                         <div className={styles.chatEmptyState}>
-                            <ChatHelpFilled fontSize={"120px"} primaryFill={"rgba(214, 39, 46, 1)"} aria-hidden="true" aria-label="Chat logo" />
+                            <Logo aria-hidden="true" aria-label="OMAX Technology logo" />
+                            {/* <ChatHelpFilled fontSize={"120px"} primaryFill={"rgba(214, 39, 46, 1)"} aria-hidden="true" aria-label="Chat logo" /> */}
                             <h1 className={styles.chatEmptyStateTitle}>Chat with your data</h1>
                             <h2 className={styles.chatEmptyStateSubtitle}>Ask anything or try an example</h2>
                             <ExampleList onExampleClicked={onExampleClicked} />
--- app/frontend/src/pages/layout/Layout.tsx
+++ app/frontend/src/pages/layout/Layout.tsx
@@ -1,7 +1,5 @@
 import { Outlet, NavLink, Link } from "react-router-dom";
 
-import github from "../../assets/github.svg";
-
 import styles from "./Layout.module.css";
 
 import { useLogin } from "../../authConfig";
